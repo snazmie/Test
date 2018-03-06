b82c05419a425cbfd68cf24f59bd66952b14663c
Syafiq <snazmie@users.noreply.github.com>
2018-03-06 23:35:42 +0800

    New file added

--- acceptbid.php
+++ acceptbid.php
@@ -4,6 +4,7 @@ include("config.php");
 $job_id=$_GET['jid'];					
 $dev_id=$_GET['did'];
 
+$sql="UPDATE bidjob set bid_status=1 WHERE job_id='$job_id' AND dev_id='$dev_id'";
 
 if (!mysqli_query($db,$sql))
 	{
